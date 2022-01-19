a3b0eedcdea3e32f6af4c33af25588996da5c055
Erno Hentonen <erno.hentonen@opiframe.com>
2022-01-19 11:00:37 +0200

    simple spa html

--- javascript/07_simple_spa/server.js
+++ javascript/07_simple_spa/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //DATABASE
 
-const database = [];
+let database = [];
 let id = 100;
 
 app.get("/api/contact",function(req,res) {
@@ -30,6 +30,31 @@ app.post("/api/contact", function(req,res) {
 	database.push(contact);
 	res.status(201).json({"message":"created"});
 })
+
+app.delete("/api/contact/:id",function(req,res) {
+	let tempId = parseInt(req.params.id,10);
+	database = database.filter(item => item.id !== tempId);
+	res.status(200).json({"message":"success"});
+})
+
+app.put("/api/contact/:id", function(req,res) {
+	let tempId = parseInt(req.params.id,10);
+	let contact = {
+		"firstname":req.body.firstname,
+		"lastname":req.body.lastname,
+		"email":req.body.email,
+		"address":req.body.address,
+		"phone":req.body.phone,
+		"id":tempId
+	}
+	for(let i=0;i<database.length;i++) {
+		if(tempId === database[i].id) {
+			database.splice(i,1,contact);
+			return res.status(200).json({"message":"success"})
+		}
+	}
+	return res.status(404).json({"message":"not found"});
+});
 
 app.listen(port);
 
