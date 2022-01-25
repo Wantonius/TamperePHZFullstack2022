3cce7ecad6f435a291ad4ccf5b1808e7f711adcb
Erno Hentonen <erno.hentonen@opiframe.com>
2022-01-25 13:55:24 +0200

    harkat

---
+++ react/08_composition/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactCard from './components/ContactCard';
+import NamedChildren from './components/NamedChildren';
+function App() {
+  return (
+    <div className="App">
+		<ContactCard>
+			<h3>Simple Contact Card</h3>
+		</ContactCard>
+		<ContactCard>
+			My Card
+		</ContactCard>
+		<NamedChildren 
+			header={<h2>Complex Contact Card</h2>}
+			media={<h3>Media here</h3>}
+			content={<p>Content here</p>}/>
+		<NamedChildren
+			header={<h3>No Media Card</h3>}
+			content={<h3>Content Area</h3>}/>
+	</div>
+  );
+}
+
+export default App;
---
+++ react/08_composition/src/components/ContactCard.js
@@ -0,0 +1,17 @@
+const ContactCard = (props) => {
+	
+	let cardStyle = {
+		backgroundColor:"lightgreen",
+		height:200,
+		width:150,
+		textAlign:"center",
+		margin:10
+	}
+	return (
+		<div style={cardStyle}>
+		{props.children}
+		</div>
+	)
+}
+
+export default ContactCard;
