0d651f0e4c67e08c70794bb2b23eebcfad8f13b0
Erno Hentonen <erno.hentonen@opiframe.com>
2022-01-25 13:02:09 +0200

    simple custom hook + routing

---
+++ react/06_router_test/src/Home.js
@@ -0,0 +1,11 @@
+
+const Home = (props) => {
+
+	return (
+		<div>
+			<h1>This is the home page of router test</h1>
+		</div>
+	)
+}
+
+export default Home;
---
+++ react/06_router_test/src/Secret.js
@@ -0,0 +1,11 @@
+
+const Secret = (props) => {
+
+	return(
+			<div>
+				<h1>Welcome to the secret page</h1>
+			</div>
+	)
+}
+
+export default Secret;
