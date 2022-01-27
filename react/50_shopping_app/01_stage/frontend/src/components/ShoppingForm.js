5270ac385e6fb401687263c85f0519e0a421d71a
Erno Hentonen <erno.hentonen@opiframe.com>
2022-01-27 10:07:37 +0200

    shopping app step 1

---
+++ react/50_shopping_app/01_stage/frontend/src/components/ShoppingForm.js
@@ -0,0 +1,67 @@
+import {useState} from 'react';
+import {Form,Button} from 'semantic-ui-react';
+
+const ShoppingForm = (props) => {
+	
+	const [state,setState] = useState({
+		type:"",
+		count:0,
+		price:0
+	});
+	
+	const onChange = (event) => {
+		setState({
+			...state,
+			[event.target.name]:event.target.value
+		})
+	};
+	
+	const onSubmit = (event) => {
+		event.preventDefault();
+		let item = {
+			...state
+		}
+		props.addToList(item);
+		setState({
+			type:"",
+			count:0,
+			price:0
+		})
+	}
+	
+	return (
+		<div style={{
+			width:500,
+			margin:"auto",
+			backgroundColor:"lightgreen"
+		}}>
+			<Form onSubmit={onSubmit}>
+				<Form.Field>
+					<label htmlFor="type">Type</label>
+					<input type="text"
+							name="type"
+							onChange={onChange}
+							value={state.type}/>
+				</Form.Field>
+				<Form.Field>
+					<label htmlFor="count">Count</label>
+					<input type="number"
+							name="count"
+							onChange={onChange}
+							value={state.count}/>
+				<Form.Field>
+					<label htmlFor="price">Price</label>
+					<input type="number"
+							name="price"
+							onChange={onChange}
+							step="0.01"
+							value={state.price}/>
+				</Form.Field>
+				<Button type="submit">Add</Button>
+				</Form.Field>
+			</Form>
+		</div>
+	)
+}
+
+export default ShoppingForm;
