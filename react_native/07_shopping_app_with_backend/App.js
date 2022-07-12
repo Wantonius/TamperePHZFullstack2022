a195e2f5b22ed8eb3a45fef627f9c17145b0c9fa
Erno Hentonen <erno.hentonen@opiframe.com>
2022-07-12 12:34:06 +0300

    with backend

--- react_native/07_shopping_app_with_backend/App.js
+++ react_native/07_shopping_app_with_backend/App.js
@@ -5,7 +5,7 @@ import ShoppingForm from './components/ShoppingForm';
 import LoginPage from './components/LoginPage';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,37 +13,147 @@ export default function App() {
 	
 	const [state,setState] = useState({
 		list:[],
-		id:100,
 		isLogged:false,
 		token:""
 	});
 	
-	const addToList = (item) => {
-		item.id = state.id;
-		setState((state) => {
-			return {
-				list:state.list.concat(item),
-				id:state.id+1
+	const [urlRequest,setUrlRequest] = useState({
+		url:"",
+		request:{},
+		action:""
+	})
+	
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			if(!urlRequest.url) {
+				return;
 			}
+			let url = "http://to-native-shopping.herokuapp.com"+urlRequest.url;
+			const response = await fetch(url,urlRequest.request);
+			if(response.ok) {
+				switch(urlRequest.action) {
+					case "register":
+						alert("Register success");
+						return;
+					case "login":
+						const data = await response.json();
+						if(!data) {
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								isLogged:true,
+								token:data.token
+							}
+						})
+						getList(data.token);
+						return;
+					case "getlist":
+						const list = await response.json();
+						if(!list) {
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								list:list
+							}
+						})
+						return;
+					case "additem":
+						getList(state.token);
+						return;
+					case "removeitem":
+						getList(state.token);
+						return;
+					default:
+						return;
+				}
+			} else {
+				if(response.status === 403) {
+					if(urlRequest.action !== "login") {
+						setState({
+							list:[],
+							isLogged:false,
+							token:""
+						})
+					}
+				}
+			}
+		}
+		
+		fetchData();
+		
+	},[urlRequest])
+	
+	//SHOPPING API
+	
+	const getList = (token) => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				method:"GET",
+				mode:"cors",
+				headers:{"Content-type":"application/json",
+				token:token}
+			},
+			action:"getlist"
+		})
+	}
+	
+	const addToList = (item) => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				method:"POST",
+				mode:"cors",
+				headers:{"Content-type":"application/json",
+				token:state.token},
+				body:JSON.stringify(item)
+			},
+			action:"additem"
 		})
 	};
 	
 	const removeFromList = (id) => {
-		setState((state) => {
-			let tempList = state.list.filter(item => item.id !== id);
-			return {
-				...state,
-				list:tempList
-			}
+		setUrlRequest({
+			url:"/api/shopping/"+id,
+			request:{
+				method:"DELETE",
+				mode:"cors",
+				headers:{"Content-type":"application/json",
+				token:state.token}
+			},
+			action:"removeitem"
 		})
 	}
 	
 	const register = (user) => {
-		
+		setUrlRequest({
+			url:"/register",
+			request:{
+				method:"POST",
+				mode:"cors",
+				headers:{"Content-type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"register"
+		})		
 	}
 	
 	const login = (user) => {
-		
+		setUrlRequest({
+			url:"/login",
+			request:{
+				method:"POST",
+				mode:"cors",
+				headers:{"Content-type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"login"
+		})			
 	}
 	
 	return (
